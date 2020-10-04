46b754305cc4e7baa01e66f711a188e499116507
Carlos Paz <pazaupazcarlossebastian@gmail.com>
2020-10-04 11:43:57 -0500

    First commit

---
+++ src/components/Imagen.vue
@@ -0,0 +1,20 @@
+<template>
+         <div class="card">
+          <img :src="imagen.url" class="card-img-top"  />
+          <div class="card-body">
+            <h5 class="card-title">{{ imagen.created_at }}</h5>
+          </div>
+        </div>
+</template>
+
+
+<script>
+export default {
+    
+    name: 'Imagen',
+    props:["imagen"],
+
+
+
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center">Imagenes</h1>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-4" v-for="imagen in imagenes" :key="imagen.id" >
+        <Imagen :imagen="imagen"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Imagen  from '../components/Imagen'
+
+
+export default {
+
+  name: "Home",
+  components:{
+    Imagen
+  },
+  data() {
+    return {
+      imagenes: [],
+    };
+  },
+  mounted() {
+    fetch("https://api.jeisontech.dev/api/images/")
+      .then((res) => res.json())
+      .then((data) => {
+        this.imagenes = data;
+      });
+  },
+  /*  beforeCreate(){
+    console.log("beforeCreate")
+  },
+  created(){
+    console.log("created")
+  },
+  beforeMount(){
+    console.log("beforeMount")
+  },
+  mounted(){
+    console.log("mounted")
+  } */
+};
+</script>
+
+<style>
+</style>
